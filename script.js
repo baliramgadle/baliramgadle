7424e93e33097009ab5aeb11e65980ae2601cd07
Baliram Gadle <baliramgadle@gmail.com>
2024-12-13 17:46:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Select the menu icon and nav links
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+const main = document.querySelector("main");
+
+// Toggle the 'active' class on the nav links
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+});
+
+main.addEventListener('click', ()=>{
+    navLinks.classList.remove('active');
+    menuIcon.classList.remove('active');
+});
